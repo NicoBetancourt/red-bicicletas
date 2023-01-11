a8a1e79246d4c7c72fe42c5126a9b5a5b0a92bc6
NicoBetancourt <jnbetancourtl@gmail.com>
2023-01-11 12:13:35 +0100

    API de bicicletas

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,7 +1,18 @@
-var map = L.map('main_map').setView([4.752466, -74.064737],13);
+var map = L.map('main_map').setView([4.752466, -74.064737], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([4.752466, -74.064737]).addTo(map);
+// L.marker([4.752466, -74.064737]).addTo(map);
+
+$.ajax({
+    type: 'GET',
+    dataType: "json",
+    url: "api/bicicletas",
+    success: (result) => {
+        result.bicicletas.forEach((bici) => {
+            L.marker(bici.ubicacion,{title: bici.id}).addTo(map);
+        });
+    }
+})
